763916c04e644d05d708b50a3ce2c96c6880b173
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2025-02-17 19:50:35 -0600

    First commit

---
+++ doggii-frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Asegura que Tailwind escanee los archivos correctos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
