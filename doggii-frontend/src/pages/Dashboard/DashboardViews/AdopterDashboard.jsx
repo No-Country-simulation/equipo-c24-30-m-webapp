2f41257f01ba10e1ca25473d1e924aceab0424b9
Abril Herrada <abrilherrada@gmail.com>
2025-02-19 19:37:52 -0300

    Added initial structure to AdopterDashboard component

--- doggii-frontend/src/pages/Dashboard/DashboardViews/AdopterDashboard.jsx
+++ doggii-frontend/src/pages/Dashboard/DashboardViews/AdopterDashboard.jsx
@@ -3,7 +3,10 @@
 const AdopterDashboard = () => {
 
   return (
-    <h2>Dashboard de adoptante</h2>
+    <div className="pl-8 pr-8">
+      <p>En esta sección, podés ver un resumen de la información importante de tu cuenta.</p>
+      <h2 className='pt-8 pb-4 text-center'>Estos peluditos buscan hogar</h2>
+    </div>
   )
 }
 
