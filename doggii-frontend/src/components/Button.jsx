4b0d3b5076c3457a3a5fe3398edb5edb30941a08
Abril Herrada <abrilherrada@gmail.com>
2025-02-19 20:58:34 -0300

    Created reusable Button component to include in dashboard and dashboard cards and implemented it

---
+++ doggii-frontend/src/components/Button.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const Button = ({ 
+  children, 
+  onClick = () => {}, 
+  className = '',
+  as: Component = 'button',
+  ...props
+}) => {
+  return (
+    <Component 
+      className={`flex items-center justify-center w-2xs p-3 font-normal tracking-wide rounded-md bg-(--primary) ${className}`}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  as: PropTypes.elementType
+};
+
+export default Button;
--- doggii-frontend/src/components/Card.jsx
+++ doggii-frontend/src/components/Card.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Card = ({image, title, description}) => {
 
   return (
-    <div className='max-w-xs rounded-md shadow-md dark:bg-gray-50'>
+    <div className='max-w-xs min-w-3xs rounded-md shadow-md dark:bg-gray-50'>
       <img src={image} alt='' className='object-cover object-center w-full rounded-t-md h-62 dark:bg-gray-500' />
       <div className='flex flex-col justify-between p-6 space-y-8'>
         <div className='space-y-2'>
           <h2>{title}</h2>
           <p>{description}</p>
         </div>
-        <button type='button' className='flex items-center justify-center w-full p-3 font-normal tracking-wide rounded-md bg-(--primary)'>Ver más</button>
+        <Button className='w-40 m-auto bg-(--secondary)'>
+          Ver más
+        </Button>
       </div>
     </div>
   )
