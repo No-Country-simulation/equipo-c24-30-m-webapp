f5f64cf50fe1b5da31d40df9f855b747b3022d75
Jmjoss <josekandia42@gmail.com>
2025-02-23 16:46:17 -0300

    implement models and interface

---
+++ backend/src/api/medical-record/model.ts
@@ -0,0 +1,12 @@
+// LIBRARIES
+import { Schema, model } from "mongoose";
+
+// MODELS
+
+const medicalRecordSchema = new Schema({
+    pet: { type: Schema.Types.ObjectId, ref: "Pet", required: true },
+    vaccinations: [{ type: String }],
+    medicalHistory: [{ type: String }],
+}, { timestamps: true });
+
+const MedicalRecord = model("MedicalRecord", medicalRecordSchema);
