d6dc3d2f85396bdfdf882711e28c95839d3142a5
Abril Herrada <abrilherrada@gmail.com>
2025-02-24 12:23:00 -0300

    Implemented User component in admin view to unify users display

--- doggii-frontend/src/pages/Dashboard/DashboardViews/AdminDashboard.jsx
+++ doggii-frontend/src/pages/Dashboard/DashboardViews/AdminDashboard.jsx
@@ -1,21 +1,18 @@
 import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
+import userDataMock from "../../../test/userDataMock.json";
 
 const AdminDashboard = () => {
+  const userNumber = userDataMock.length;
 
   return (
     <div className="pl-8 pr-8">
       <p>Te damos la bienvenida al panel de administración. Acá, podés ver un resumen de la información importante de la aplicación.</p>
-      <div className='grid lg:grid-cols-2 md:grid-cols-1 place-items-center gap-20 py-10 px-20 sm:px-10'>
+      <div className='grid lg:grid-cols-2 md:grid-cols-1 gap-10 py-10 px-20 sm:px-10'>
         <div className='p-6 w-full rounded-md shadow-md dark:bg-gray-50 flex flex-col items-center'>
-          <h2 className='mb-4'>Adoptantes</h2>
-          <p className=''>Hay 45 usuarios registrados.</p>
-          <Button as={Link} to='/adoptantes' className='w-40 mt-8'>Ver todos</Button>
-        </div>
-        <div className='p-6 w-full rounded-md shadow-md dark:bg-gray-50 flex flex-col items-center'>
-          <h2 className='mb-4'>Refugios</h2>
-          <p className=''>Hay 58 refugios registrados.</p>
-          <Button as={Link} to='/refugios' className='w-40 mt-8'>Ver todos</Button>
+          <h2 className='mb-4'>Usuarios</h2>
+          <p className=''>Hay {userNumber} usuarios registrados.</p>
+          <Button as={Link} to='/usuarios' className='w-40 mt-8'>Ver todos</Button>
         </div>
         <div className='p-6 w-full rounded-md shadow-md dark:bg-gray-50 flex flex-col items-center'>
           <h2 className='mb-4'>Mascotas</h2>
