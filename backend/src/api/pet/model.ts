04241bb09da4ae56a3acfbcb031433d292f7d681
Keys4Ever <cubillagonzalo@outlook.com>
2025-02-24 12:36:32 -0300

    Add pets basic crud

--- backend/src/api/pet/model.ts
+++ backend/src/api/pet/model.ts
@@ -1,21 +1,24 @@
 // LIBRARIES
-import { Schema, model } from "mongoose";
+import { Document, Schema, model } from "mongoose";
 
 // MODELS
-import User from "../user/model";
-import Adopter from "../adopter/model";
+import { IPet, PetStatus, PetType } from "./interface";
 
 // PET MODEL
 const petSchema = new Schema({
     name: { type: String, required: true },
-    species: { type: String, required: true },
+    species: { type: String, enum: Object.values(PetType), required: true },
     breed: { type: String },
+    description: { type: String },
     age: { type: Number, required: true },
+    images: [{ type: String }],
     gender: { type: String, enum: ["Male", "Female"], required: true },
     healthStatus: { type: String, default: "Healthy" },
     shelter: { type: Schema.Types.ObjectId, ref: "Shelter" },
     adopter: { type: Schema.Types.ObjectId, ref: "Adopter", default: null },
+    status: { type: String, enum: Object.values(PetStatus), default: PetStatus.AVAILABLE},
     available: { type: Boolean, default: true },
 }, { timestamps: true });
 
-const Pet = model("Pet", petSchema);
+const Pet = model<IPet & Document>("Pet", petSchema); 
+export default Pet;
