e786c426769d403e700567f5a321d9ef2908c5eb
Abril Herrada <abrilherrada@gmail.com>
2025-03-05 14:21:16 -0300

    Removed AdopterAdditionalProfileForm component from AdopterProfile as it is no longer needed

--- doggii-frontend/src/pages/Profile/ProfileViews/AdopterProfile.jsx
+++ doggii-frontend/src/pages/Profile/ProfileViews/AdopterProfile.jsx
@@ -1,5 +1,4 @@
 import BasicProfileForm from "../../../components/Forms/BasicProfileForm";
-import AdopterAdditionalProfileForm from "../../../components/Forms/AdopterAdditionalProfileForm";
 
 const AdopterProfile = () => {
 
@@ -7,7 +6,6 @@ const AdopterProfile = () => {
     <div className="pl-8 pr-8">
       <p>Estos son tus datos personales. Recordá que, para poder iniciar una solicitud de adopción, los datos tienen que estar completados.</p>
       <BasicProfileForm title='Datos personales' description='Estos son los datos básicos que debés completar para poder iniciar una solicitud de adopción.'/>
-      <AdopterAdditionalProfileForm />
     </div>
   )
 }
