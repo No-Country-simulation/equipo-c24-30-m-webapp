ec28f3265a89656627d24e1aa214ddcc1597f261
Jmjoss <josekandia42@gmail.com>
2025-03-06 14:19:34 -0300

    add shelterEmail and shelterPhone

--- backend/src/api/shelter/model.ts
+++ backend/src/api/shelter/model.ts
@@ -9,7 +9,18 @@ const Shelter = User.discriminator(
     "Shelter",
     new Schema<IShelter>({
         shelterName: { type: String, required: true, trim: true },
-        verified: { type: Boolean, default: false }, 
+        verified: { type: Boolean, default: false },
+        shelterEmail: { 
+            type: String, 
+            trim: true,
+            unique: true,
+            lowercase: true,
+            match: [
+                /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                "Please provide a valid email",
+            ],
+         }, 
+        shelterPhone: { type: String, trim: true },
     })
 );
 
