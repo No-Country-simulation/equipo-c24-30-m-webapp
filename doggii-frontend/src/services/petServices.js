94da83e52fd0d57ce80e8b9422a744a868999085
Abril Herrada <abrilherrada@gmail.com>
2025-03-07 10:28:25 -0300

    Created update pet service

--- doggii-frontend/src/services/petServices.js
+++ doggii-frontend/src/services/petServices.js
@@ -1,24 +1,30 @@
 import axios from 'axios';
 
-const getPet = async (petId) => {
-  const response = await axios.get(`${import.meta.env.VITE_BACKEND_URI}/api/pet/${petId}`);
-  return response.data;
-}
-
 const createPet = async (pet) => {
   const response = await axios.post(`${import.meta.env.VITE_BACKEND_URI}/api/pet`, pet);
   return response.data;
 };
+
+const getPet = async (petId) => {
+  const response = await axios.get(`${import.meta.env.VITE_BACKEND_URI}/api/pet/${petId}`);
+  return response.data;
+}
 
 const getPetsByShelter = async (shelterId) => {
   const response = await axios.get(`${import.meta.env.VITE_BACKEND_URI}/api/pet/shelter/${shelterId}`);
   return response.data;
 }
 
+const updatePet = async (petId, pet) => { 
+  const response = await axios.put(`${import.meta.env.VITE_BACKEND_URI}/api/pet/${petId}`, pet);
+  return response.data;
+}
+
 const petServices = {
   createPet,
   getPetsByShelter,
-  getPet
+  getPet,
+  updatePet
 };
 
 export default petServices;
