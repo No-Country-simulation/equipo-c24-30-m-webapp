edc2d967f2703c37c3e55c5070b33a5fd5ad6608
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2025-03-07 09:47:44 -0600

    Display adoption requests or a message when none are available in AdopterApplications

--- doggii-frontend/src/pages/Applications/ApplicationViews/AdopterApplications.jsx
+++ doggii-frontend/src/pages/Applications/ApplicationViews/AdopterApplications.jsx
@@ -9,8 +9,9 @@ import { jwtDecode } from "jwt-decode";
 const AdopterApplications = () => {
   const [visibleItems, setVisibleItems] = useState(6);
   const [applications, setApplications] = useState(applicationsDataMock.slice(0, visibleItems));
-  const navigate = useNavigate();
   const [petsData, setPetsData] = useState([]);
+  const [adoptionRequest, setAdoptionRequest] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAdoptionRequestsAndPets = async () => {
@@ -61,9 +62,11 @@ const AdopterApplications = () => {
           // Guardar los datos para renderizarlos
           setPetsData(combinedPetsDetails);
         } else {
+          setAdoptionRequest(false);
           console.warn('No se encontraron solicitudes de adopción.');
         }
       } catch (error) {
+        setAdoptionRequest(false);
         console.error('Error al obtener solicitudes de adopción o detalles de mascotas:', error);
       }
     };
@@ -86,19 +89,23 @@ const AdopterApplications = () => {
   return (
     <div className='pl-8 pr-8'>
       <p>Consultá todas las solicitudes de adopción que iniciaste y sus estados.</p>
-      <div className='flex flex-wrap justify-center gap-6 pt-8'>
-         {applications.map((application, index) => (
-                  <HorizontalCard key={index} id={application.id} subtitle='Estado' text={application.status} image={application.photo} title={application.petName} description={`Refugio: ${application.shelterName}`} onSee={() => handleGoToApplicationDetails(application.id)}/>
-          ))}
-      </div>
-      {visibleItems < applications.length && (
-        <Button
-          className='m-auto mt-8 text-xl'
-          onClick={handleSeeMore}
-        >
-          Ver más
-        </Button>
-      )}
+      {adoptionRequest ? 
+      <div>
+        <div className='flex flex-wrap justify-center gap-6 pt-8'>
+          {applications.map((application, index) => (
+                    <HorizontalCard key={index} id={application.id} subtitle='Estado' text={application.status} image={application.photo} title={application.petName} description={`Refugio: ${application.shelterName}`} onSee={() => handleGoToApplicationDetails(application.id)}/>
+            ))}
+        </div>
+        {visibleItems < applications.length && (
+          <Button
+            className='m-auto mt-8 text-xl'
+            onClick={handleSeeMore}
+          >
+            Ver más
+          </Button>
+        )}
+      </div> : <p className='flex justify-center mt-10'>No hay solicitudes de adopción.</p>}
+      
     </div>
   )
 }
