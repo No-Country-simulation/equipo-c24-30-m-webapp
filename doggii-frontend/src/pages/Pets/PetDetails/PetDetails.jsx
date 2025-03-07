e8fc6b1e64fbf2b8d82381aa235f2060dfd27b2f
Abril Herrada <abrilherrada@gmail.com>
2025-03-06 21:17:46 -0300

    Modified PetDetails to replace mock data usage with real pet data consumption, added error and loading handling

--- doggii-frontend/src/pages/Pets/PetDetails/PetDetails.jsx
+++ doggii-frontend/src/pages/Pets/PetDetails/PetDetails.jsx
@@ -1,18 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import Button from '../../../components/Button';
 import getTimeElapsed from '../../../utils/getTimeElapsed';
-import petDataMock from '../../../test/petsDataMock.json';
+import petServices from '../../../services/petServices';
 
 const PetDetails = () => {
   const userRole = useSelector((state) => state.user.role);
   const navigate = useNavigate();
-
   const petId = useParams().id;
-  const pet = petDataMock.find((pet) => pet.id === petId);
-  const age = pet.age.years > 0 ? `${pet.age.years} años` : 
-              (pet.age.months > 0 ? `${pet.age.months} meses` :
-              (pet.age.days > 0 ? `${pet.age.days} días` : ''));
+  const [pet, setPet] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleGetPet = useCallback(async () => {
+    try {
+      setError(null);
+      setIsLoading(true);
+      const response = await petServices.getPet(petId);
+      setPet(response.payload);
+    } catch (error) {
+      console.log('Error fetching pet:', error);
+      setError('No pudimos cargar la mascota. Tocá el botón para intentar de nuevo.');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [petId]);
+
+  useEffect(() => {
+    handleGetPet();
+  }, [handleGetPet]);
 
   const handleGoBack = () => {
     navigate(-1);
@@ -22,72 +39,110 @@ const PetDetails = () => {
     <div className='p-8'>
       <div className='flex items-center gap-6'>
         <button onClick={handleGoBack} className='text-5xl text-(--secondary) cursor-pointer'>←</button>
-        <h1 className='text-5xl'>{pet.name}</h1>
+        <h1 className='text-5xl'>{pet?.name}</h1>
       </div>
-      <div className='py-10 grid lg:grid-cols-5 sm:grid-cols-1 gap-10'>
-        <div className='lg:col-span-2'>
-          <img src={pet.photo} alt={pet.name} className='min-h-110 object-cover rounded-xl'/>
+      {isLoading ? (
+        <div className='flex items-center justify-center h-150'>
+          <div className='animate-spin rounded-full h-30 w-30 my-auto border-b-8 border-(--secondary)'></div>
         </div>
-        <div className='lg:col-span-3 flex flex-col justify-between py-2'>
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Sexo: </span>
-            <span>{pet.sex}</span>
-          </p>
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Edad: </span>
-            <span>{age}</span>
-          </p>
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Tamaño: </span>
-            <span>{pet.size}</span>
-          </p>
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Vacunación: </span>
-            <span>{pet.vaccinated ? 'Al día' : 'Faltan vacunas'}</span>
-          </p>
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Castración: </span>
-            <span>{pet.neutered ? (pet.sex === 'macho' ? 'Castrado' : 'Castrada') : 'Pendiente'}</span>
-          </p>
-          {pet.specialCare && (
-            <p className='pb-2 text-xl'>
-              <span className='font-medium'>Cuidados especiales: </span>
-              <span>{pet.specialCare}</span>
-            </p>
-          )}
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Descripción: </span>
-            <span>{pet.description}</span>
-          </p>
-          {userRole === "adopter" && (
-            <p className='pb-2 text-xl'>
-              <span className='font-medium'>Refugio: </span>
-              <span>{pet.shelterName}</span>
-            </p>
-          )}
-          <p className='pb-2 text-xl'>
-            <span className='font-medium'>Publicado hace: </span>
-            <span>{getTimeElapsed(pet.createdAt)}</span>
-          </p>
+      ) : (
+      error ? (
+        <>
+        <div className="flex items-center justify-center max-w-3xl p-6 space-x-4 mx-auto my-10 rounded-md bg-red-100">
+          <div className="flex items-center self-stretch justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10">
+              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path>
+            </svg>
+          </div>
+          <span className='text-lg'>{error}</span>
         </div>
-      </div>
-      {userRole === "shelter" ?
-        <div className='flex justify-around'>
-          <Button className='text-2xl w-50'>
-            Eliminar
-          </Button>
-          <Button className='text-2xl w-50'>
-            Editar
-          </Button>
-          <Button className='text-2xl w-50'>
-            {pet.available ? 'Pausar' : 'Reanudar'}
-          </Button>
-        </div>
-        :
-        <Button className='text-2xl mx-auto w-60'>
-          Adoptar
+        <Button 
+          onClick={handleGetPet}
+          className="mx-auto w-50 text-lg"
+        >
+          Reintentar
         </Button>
-      }
+        </>
+      ) : (
+      <div className='py-10 grid lg:grid-cols-5 sm:grid-cols-1 gap-10'>
+        {pet && (
+          <>
+            <div className='lg:col-span-2'>
+              <div className='w-full aspect-square relative'>
+                <img 
+                  src={pet.photos[0]} 
+                  alt={pet.name} 
+                  className='absolute inset-0 w-full h-full object-cover rounded-xl'
+                />
+              </div>
+            </div>
+            <div className='lg:col-span-3 flex flex-col justify-between py-2'>
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Sexo: </span>
+                <span>{pet.sex === 'male' ? 'macho' : (pet.sex === 'female' ? 'hembra' : '')}</span>
+              </p>
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Edad: </span>
+                <span>
+                  {pet.age.years > 0 ? `${pet.age.years} años` : 
+                    (pet.age.months > 0 ? `${pet.age.months} meses` :
+                      (pet.age.days > 0 ? `${pet.age.days} días` : ''))}
+                </span>
+              </p>
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Tamaño: </span>
+                <span>{pet.size}</span>
+              </p>
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Vacunación: </span>
+                <span>{pet.vaccinated ? 'al día' : 'faltan vacunas'}</span>
+              </p>
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Castración: </span>
+                <span>{pet.neutered ? (pet.sex === 'macho' ? 'castrado' : 'castrada') : 'pendiente'}</span>
+              </p>
+              {pet.specialCare && (
+                <p className='pb-2 text-xl'>
+                  <span className='font-medium'>Cuidados especiales: </span>
+                  <span>{pet.specialCare}</span>
+                </p>
+              )}
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Descripción: </span>
+                <span>{pet.description}</span>
+              </p>
+              {userRole === "adopter" && (
+                <p className='pb-2 text-xl'>
+                  <span className='font-medium'>Refugio: </span>
+                  <span>{pet.shelterName}</span>
+                </p>
+              )}
+              <p className='pb-2 text-xl'>
+                <span className='font-medium'>Publicado hace: </span>
+                <span>{getTimeElapsed(pet.createdAt)}</span>
+              </p>
+            </div>
+            {userRole === "shelter" ?
+              <div className='flex justify-around col-span-5'>
+                <Button className='text-2xl w-50'>
+                  Eliminar
+                </Button>
+                <Button className='text-2xl w-50'>
+                  Editar
+                </Button>
+                <Button className='text-2xl w-50'>
+                  {pet.available ? 'Pausar' : 'Reanudar'}
+                </Button>
+              </div>
+              :
+              <Button className='text-2xl mx-auto w-60 col-span-5'>
+                Adoptar
+              </Button>
+            }
+          </>
+        )}
+      </div>
+      ))}
     </div>
   )
 }
