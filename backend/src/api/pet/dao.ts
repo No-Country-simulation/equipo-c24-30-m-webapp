afa59a7756508edf3522a8ac99916968c2d908f0
Jmjoss <josekandia42@gmail.com>
2025-03-08 10:51:09 -0300

    add validation in pet, fix service and add authentication

--- backend/src/api/pet/dao.ts
+++ backend/src/api/pet/dao.ts
@@ -2,44 +2,72 @@ import { Model, FilterQuery } from "mongoose";
 import { IPet } from "./interface";
 
 class PetDAO {
-    private model: Model<IPet>;
+  private model: Model<IPet>;
 
-    constructor(model: Model<IPet>) {
-        this.model = model;
-    }
+  constructor(model: Model<IPet>) {
+    this.model = model;
+  }
 
-    async create(data: Partial<IPet>): Promise<IPet> {
-        return await this.model.create(data);
-    }
+  async create(data: Partial<IPet>): Promise<IPet> {
+    return await this.model.create(data);
+  }
 
-    async read(id: string): Promise<IPet | null> {
-        return await this.model.findById(id).populate("shelter adopter").lean();
-    }
+  async read(id: string): Promise<IPet | null> {
+    return await this.model
+      .findById(id)
+      .populate({
+        path: "shelter",
+        select: "-password -__v",
+      })
+      .populate({
+        path: "adopter",
+        select: "-password -__v",
+      })
+      .lean();
+  }
 
-    async update(id: string, data: Partial<IPet>): Promise<IPet | null> {
-        console.log(data);
-        return await this.model
-            .findByIdAndUpdate(id, data, { new: true })
-    }
+  async update(id: string, data: Partial<IPet>): Promise<IPet | null> {
+    console.log(data);
+    return await this.model.findByIdAndUpdate(id, data, { new: true });
+  }
 
-    async delete(id: string): Promise<IPet | null> {
-        return await this.model.findByIdAndDelete(id).lean();
-    }
+  async delete(id: string): Promise<IPet | null> {
+    return await this.model.findByIdAndDelete(id).lean();
+  }
 
-    async find(query: FilterQuery<IPet>): Promise<IPet[]> {
-        return await this.model.find(query).populate("shelter adopter").lean();
-    }
+  async find(query: FilterQuery<IPet>): Promise<IPet[]> {
+    return await this.model
+      .find(query)
+      .populate("shelter adopter")
+      .populate({
+        path: "shelter",
+        select: "-password -__v",
+      })
+      .populate({
+        path: "adopter",
+        select: "-password -__v",
+      })
+      .lean();
+  }
 
-    // i could not find a way to do the address filter without this method. If you find a way feel free to rewrite it.
-    async findWithShelterMatch(query: FilterQuery<IPet>, shelterMatch: any): Promise<IPet[]> {
-        return await this.model.find(query)
-           .populate({
-              path: 'shelter',
-              match: shelterMatch
-           })
-           .populate("adopter")
-           .lean();
-    }
+  // i could not find a way to do the address filter without this method. If you find a way feel free to rewrite it.
+  async findWithShelterMatch(
+    query: FilterQuery<IPet>,
+    shelterMatch: any
+  ): Promise<IPet[]> {
+    return await this.model
+      .find(query)
+      .populate({
+        path: "shelter",
+        match: shelterMatch,
+        select: "-password -__v",
+      })
+      .populate({
+        path: "adopter",
+        select: "-password -__v",
+      })
+      .lean();
+  }
 }
 
 export default PetDAO;
