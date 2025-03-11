f9571485284dca728c26c265c9e7496e0d151a6d
Abril Herrada <abrilherrada@gmail.com>
2025-03-11 09:23:05 -0300

    Added AdopterAdoptionForm Route to App.jsx

--- doggii-frontend/src/App.jsx
+++ doggii-frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import Profile from './pages/Profile/Profile';
 import Pets from './pages/Pets/Pets';
 import ShelterAdoptionForm from './pages/AdoptionForm/ShelterForm/ShelterAdoptionForm.jsx';
+import AdopterAdoptionForm from './pages/AdoptionForm/AdopterForm/AdopterAdoptionForm.jsx';
 import Applications from './pages/Applications/Applications';
 import ApplicationDetail from './pages/Applications/ApplicationViews/AplicationDetail.jsx';
 import Users from './pages/Users/Users';
@@ -37,6 +38,7 @@ function App() {
           <Route path='/profile' element={<Profile />} />
           <Route path='/pets' element={<Pets />} />
           <Route path='/adoption-form' element={<ShelterAdoptionForm />} />
+          <Route path='/adoption-form/:id' element={<AdopterAdoptionForm />} />
           <Route path='/applications' element={<Applications />} />
           <Route path='/application-manage/:id' element={<ApplicationManage />} />
           <Route path='/adoption-request/:id' element={<ApplicationDetail />} />
