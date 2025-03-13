5e5351a3c65f2b2c8bd445fef07a8a94c6107892
Abril Herrada <abrilherrada@gmail.com>
2025-03-13 00:09:36 -0300

    Modified Button styles

--- doggii-frontend/src/components/Button.jsx
+++ doggii-frontend/src/components/Button.jsx
@@ -9,7 +9,21 @@ const Button = ({
 }) => {
   return (
     <Component 
-      className={`flex items-center justify-center w-2xs p-3 font-medium tracking-wide rounded-md bg-(--primary) ${className}`}
+      className={`
+        flex items-center justify-center
+        px-10 py-4
+        text-lg
+        bg-[#FFDC64]
+        border-2 border-[#E6C65A]
+        rounded-[50px]
+        cursor-pointer
+        transition-all duration-200
+        shadow-[inset_4px_4px_10px_#bea132,inset_-4px_-4px_10px_#fdf9d4]
+        hover:shadow-[inset_2px_2px_5px_#bea132,inset_-2px_-2px_5px_#fdf9d4,2px_2px_5px_#fdf9d4,-2px_-2px_5px_#fdf9d4]
+        focus:shadow-[inset_2px_2px_5px_#bea132,inset_-2px_-2px_5px_#fdf9d4,2px_2px_5px_#fdf9d4,-2px_-2px_5px_#fdf9d4]
+        focus:outline-none
+        ${className}
+      `}
       onClick={onClick}
       {...props}
     >
