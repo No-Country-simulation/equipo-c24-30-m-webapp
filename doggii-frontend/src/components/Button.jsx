64207deb60b07fe0c9fb0ffcf1b2fc682e5f5daa
Abril Herrada <abrilherrada@gmail.com>
2025-03-13 00:21:44 -0300

    Modified Button styles

--- doggii-frontend/src/components/Button.jsx
+++ doggii-frontend/src/components/Button.jsx
@@ -18,9 +18,9 @@ const Button = ({
         rounded-[50px]
         cursor-pointer
         transition-all duration-200
-        shadow-[inset_4px_4px_10px_#bea132,inset_-4px_-4px_10px_#fdf9d4]
-        hover:shadow-[inset_2px_2px_5px_#bea132,inset_-2px_-2px_5px_#fdf9d4,2px_2px_5px_#fdf9d4,-2px_-2px_5px_#fdf9d4]
-        focus:shadow-[inset_2px_2px_5px_#bea132,inset_-2px_-2px_5px_#fdf9d4,2px_2px_5px_#fdf9d4,-2px_-2px_5px_#fdf9d4]
+        shadow-[inset_4px_4px_10px_#C7A74A,inset_-4px_-4px_10px_#fdf9d4]
+        hover:shadow-[inset_2px_2px_5px_#C7A74A,inset_-2px_-2px_5px_#fdf9d4,2px_2px_5px_#C7A74A,-2px_-2px_5px_#fdf9d4]
+        focus:shadow-[inset_2px_2px_5px_#C7A74A,inset_-2px_-2px_5px_#fdf9d4,2px_2px_5px_#C7A74A,-2px_-2px_5px_#fdf9d4]
         focus:outline-none
         ${className}
       `}
