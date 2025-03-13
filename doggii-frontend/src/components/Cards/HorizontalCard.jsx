bd3b20e2c95d52525f29be5e5cf2fcc7442143e7
Abril Herrada <abrilherrada@gmail.com>
2025-03-13 15:12:27 -0300

    Modified HorizontalCard and VerticalCard styles

--- doggii-frontend/src/components/Cards/HorizontalCard.jsx
+++ doggii-frontend/src/components/Cards/HorizontalCard.jsx
@@ -6,11 +6,11 @@ const HorizontalCard = ({image, title, subtitle1, subtitle2, text1, text2, onSee
   const userRole = useSelector(state => state.user.role);
 
   return (
-    <div className="w-100 mx-auto rounded-md shadow-md bg-gray-100">
-      <div className="flex flex-row">
-        <img src={image} alt="" className="w-20 h-20 m-4 object-cover rounded-full dark:bg-gray-500"/>
-        <div className="flex flex-col justify-center flex-1 gap-4 p-4">
-          <h3 className='text-2xl'>{title}</h3>
+    <div className="w-100 mx-auto rounded-xl shadow-xl bg-(--accent-dark) p-6">
+      <div className="flex items-center justify-around gap-4">
+        <img src={image} alt="" className="w-20 h-40 object-cover rounded-full bg-gray-500"/>
+        <div className="flex flex-col justify-center gap-2">
+          <h3 className='text-2xl mb-1'>{title}</h3>
           <p >
             <span className="font-medium">{subtitle1}:&nbsp;</span>
             <span>{text1}</span>
@@ -19,18 +19,17 @@ const HorizontalCard = ({image, title, subtitle1, subtitle2, text1, text2, onSee
             <span className="font-medium">{subtitle2}:&nbsp;</span>
             <span>{text2}</span>
           </p>
+          {userRole === 'shelter' ?
+            <Button onClick={onSee} className='mx-auto mt-4 btn-secondary cursor-pointer'>
+              Gestionar
+            </Button>
+            :
+            <Button onClick={onSee}  className='mx-auto mt-4 btn-secondary cursor-pointer'>
+              Ver más
+            </Button>
+          }
         </div>
       </div>
-
-      {userRole === 'shelter' ?
-        <Button onClick={onSee} className='w-40 mx-auto mb-4 bg-(--secondary) cursor-pointer'>
-          Gestionar
-        </Button>
-        :
-        <Button onClick={onSee}  className='w-40 mx-auto mb-4 bg-(--secondary) cursor-pointer'>
-          Ver más
-        </Button>
-      }
     </div>
   )
 }
