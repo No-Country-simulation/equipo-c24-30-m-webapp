df77bd9bbdb5e837d68cffe7588bc7619dd68df4
Abril Herrada <abrilherrada@gmail.com>
2025-03-13 10:11:13 -0300

    Modified FormAdopter styles

--- doggii-frontend/src/pages/FormAdopter.jsx
+++ doggii-frontend/src/pages/FormAdopter.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { loginSuccess } from "../redux/slices/authSlice";
 import { setUserInfo } from "../redux/slices/userSlice";
+import Button from "../components/Button";
 
 const FormAdopter = () => {
   const [formData, setFormData] = useState({
@@ -99,46 +100,56 @@ const FormAdopter = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat bg-[url(/src/assets/images/login-background.jpeg)] bg-black/50 bg-opacity-10 bg-blend-multiply">
+      <div className="max-w-xl min-w-md p-10 rounded-xl bg-(--accent) shadow-2xl">
         <button
           onClick={() => navigate("/options-forms")}
-          className="text-blue-500 mb-4"
+          className="text-(--secondary-dark) hover:text-(--secondary) text-base self-start"
         >
           ← Regresar
         </button>
-        <h2 className="text-2xl font-bold mb-4 text-center">Registro para adoptar</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <div>
+        <img src="/src/assets/logo/inline-logo.png" alt="Logo" className="w-50 h-auto mx-auto mb-4"/>
+        <h1 className="text-2xl font-bold mb-4 text-center">Registro para adoptar</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className='space-y-1'>
+            <label htmlFor='userName' className='block text-sm font-medium'>
+              Nombre completo
+            </label>
             <input
               type="text"
               name="userName"
               placeholder="Nombre completo"
               value={formData.userName}
               onChange={handleChange}
-              className="border p-2 w-full rounded"
+              className="w-full px-3 py-2 text-base font-normal rounded-md border border-gray-300 focus:outline-none focus:ring-3 focus:ring-(--secondary-dark) focus:border-white"
             />
             {errors.userName && <p className="text-red-500 text-sm">{errors.userName}</p>}
           </div>
-          <div>
+          <div className='space-y-1'>
+            <label htmlFor='email' className='block text-sm font-medium'>
+              Correo electrónico
+            </label>
             <input
               type="email"
               name="email"
               placeholder="Correo electrónico"
               value={formData.email}
               onChange={handleChange}
-              className="border p-2 w-full rounded"
+              className="w-full px-3 py-2 text-base font-normal rounded-md border border-gray-300 focus:outline-none focus:ring-3 focus:ring-(--secondary-dark) focus:border-white"
             />
             {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
           </div>
-          <div>
+          <div className='space-y-1'>
+            <label htmlFor='password' className='block text-sm font-medium'>
+              Contraseña
+            </label>
             <input
               type="password"
               name="password"
               placeholder="Contraseña"
               value={formData.password}
               onChange={handleChange}
-              className="border p-2 w-full rounded"
+              className="w-full px-3 py-2 text-base font-normal rounded-md border border-gray-300 focus:outline-none focus:ring-3 focus:ring-(--secondary-dark) focus:border-white"
             />
             {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
           </div>
@@ -149,7 +160,7 @@ const FormAdopter = () => {
               placeholder="Repetir contraseña"
               value={formData.confirmPassword}
               onChange={handleChange}
-              className="border p-2 w-full rounded"
+              className="w-full px-3 py-2 text-base font-normal rounded-md border border-gray-300 focus:outline-none focus:ring-3 focus:ring-(--secondary-dark) focus:border-white"
             />
             {errors.confirmPassword && (
               <p className="text-red-500 text-sm">{errors.confirmPassword}</p>
@@ -161,10 +172,11 @@ const FormAdopter = () => {
               name="termsAccepted"
               checked={formData.termsAccepted}
               onChange={handleChange}
+              className="shrink-0 rounded-md accent-(--primary) checked:bg-(--primary) checked:hover:bg-(--primary) focus:ring-(--primary)"
             />
-            <label>
-              Al crear una cuenta aceptas los{" "}
-              <a href="/terms" className="text-blue-500 underline">
+            <label className="block font-medium">
+              Al crear una cuenta aceptás los{" "}
+              <a href="/terms" className="text-(--secondary-dark) hover:text-(--secondary) underline">
                 Términos y condiciones
               </a>
             </label>
@@ -172,12 +184,12 @@ const FormAdopter = () => {
           {errors.termsAccepted && (
             <p className="text-red-500 text-sm">{errors.termsAccepted}</p>
           )}
-          <button
+          <Button
             type="submit"
-            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+            className="mx-auto text-base"
           >
             Crear cuenta
-          </button>
+          </Button>
         </form>
       </div>
     </div>
