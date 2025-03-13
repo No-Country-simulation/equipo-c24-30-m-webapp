e4abe0b66e722cbd131578a3b9927ca17bb5297d
Abril Herrada <abrilherrada@gmail.com>
2025-03-13 15:50:52 -0300

    Modified Profile, AdopterProfile and ShelterProfile styles

--- doggii-frontend/src/pages/Profile/ProfileViews/AdopterProfile.jsx
+++ doggii-frontend/src/pages/Profile/ProfileViews/AdopterProfile.jsx
@@ -3,8 +3,8 @@ import BasicProfileForm from "../../../components/Forms/BasicProfileForm";
 const AdopterProfile = () => {
 
   return(
-    <div className="pl-8 pr-8">
-      <p>Estos son tus datos personales. Recordá que, para poder iniciar una solicitud de adopción, los datos tienen que estar completados.</p>
+    <div className="px-8">
+      <p className='text-lg'>Estos son tus datos personales. Recordá que, para poder iniciar una solicitud de adopción, los datos tienen que estar completados.</p>
       <BasicProfileForm title='Datos personales' description='Estos son los datos básicos que debés completar para poder iniciar una solicitud de adopción.'/>
     </div>
   )
