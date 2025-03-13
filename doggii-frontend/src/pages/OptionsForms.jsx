3405de0fd7677fede3671b3d1d81e59b87f203fd
Abril Herrada <abrilherrada@gmail.com>
2025-03-13 09:33:12 -0300

    Modified OptionsForms styles

--- doggii-frontend/src/pages/OptionsForms.jsx
+++ doggii-frontend/src/pages/OptionsForms.jsx
@@ -4,37 +4,40 @@ const OptionsForms = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat bg-[url(/src/assets/images/login-background.jpeg)] bg-black/50 bg-opacity-10 bg-blend-multiply">
+      <div className='flex flex-col items-center justify-center max-w-2xl min-w-sm p-10 rounded-xl bg-(--accent) shadow-2xl'>
         <button
           onClick={() => navigate("/login")}
-          className="text-blue-500 mb-4"
+          className="text-(--secondary-dark) hover:text-(--secondary) text-base self-start"
         >
           ← Regresar
         </button>
-      <h1 className="text-3xl font-bold mb-6 text-gray-800 text-center">
-        ¿Cómo quieres registrarte?
-      </h1>
-      <div className="flex gap-6">
-        {/* Card Adopter */}
-        <div
-          className="bg-white p-6 rounded-lg shadow-lg w-72 cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-xl"
-          onClick={() => navigate("/form-adopter")}
-        >
-          <h2 className="text-xl font-semibold text-center mb-2">Como adoptante</h2>
-          <p className="text-gray-600 text-center">
-            Soy una persona que quiere adoptar una mascota.
-          </p>
-        </div>
+        <img src="/src/assets/logo/inline-logo.png" alt="Logo" className="w-50 h-auto mx-auto mb-4"/>
+        <h1 className="text-2xl font-bold mb-4">
+          ¿Cómo querés registrarte?
+        </h1>
+        <div className="flex flex-col mx-auto md:flex-row gap-4">
+          {/* Card Adopter */}
+          <div
+            className="bg-(--secondary-ultralight) p-3 rounded-lg shadow-lg w-3xs cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-xl hover:bg-(--secondary-light)"
+            onClick={() => navigate("/form-adopter")}
+          >
+            <h2 className="text-lg font-semibold text-center mb-2">Como adoptante</h2>
+            <p className="text-base font-light text-center">
+              Soy una persona que quiere adoptar una mascota.
+            </p>
+          </div>
 
-        {/* Card Shelter */}
-        <div
-          className="bg-white p-6 rounded-lg shadow-lg w-72 cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-xl"
-          onClick={() => navigate("/form-shelter")}
-        >
-          <h2 className="text-xl font-semibold text-center mb-2">Como refugio</h2>
-          <p className="text-gray-600 text-center">
-            Soy el responsable de un refugio de animales.
-          </p>
+          {/* Card Shelter */}
+          <div
+            className="bg-(--secondary-ultralight) p-3 rounded-lg shadow-lg w-3xs cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-xl hover:bg-(--secondary-light)"
+            onClick={() => navigate("/form-shelter")}
+          >
+            <h2 className="text-lg font-semibold text-center mb-2">Como refugio</h2>
+            <p className="text-base font-light text-center">
+              Soy el responsable de un refugio de animales.
+            </p>
+          </div>
         </div>
       </div>
     </div>
